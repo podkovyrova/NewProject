a7c51c757d8572e553f27bafdadbbb4684ead983
podkovyrova <podkovyrova.es@mail.ru>
2023-05-31 17:34:41 +0300

    add null errors

--- src/main/java/org/podkovyrova/Main.java
+++ src/main/java/org/podkovyrova/Main.java
@@ -4,9 +4,32 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello from Java!");
+        Integer value = 5;
+        double sum = 10.5;
+        String text = "Hello Java";
+
+        value = null;
+        text = null;
+
+        if(value > 5) {
+            System.out.println(">5");
+        }
+
+        switch (value) {
+            case 1: {
+                break;
+            }
+            case 5: {
+                System.out.println("5555");
+                break;
+            }
+            case 10: {
+                break;
+            }
+            default: {
+
+            }
+        }
     }
 
-        public static int test(String text) {
-            return 5;
-        }
 }
--- src/main/kotlin/org/podkovyrova/Main.kt
+++ src/main/kotlin/org/podkovyrova/Main.kt
@@ -3,6 +3,32 @@ package org.podkovyrova
 fun main(args: Array<String>) {
     println("Hello from Kotlin!")
     test("dkkd")
+
+    var value: Int = 5
+    var sum: Double = 10.5
+    var text: String = "Hello Kotlin"
+
+    value = null;
+    text = null;
+
+    if (value > 5) {
+        println(">5")
+    }
+
+    when(value) {
+        in 1..4 -> {
+
+        }
+        5, 6 -> {
+            println("5555")
+        }
+        10 -> {
+
+        } else -> {
+
+        }
+    }
+
 }
 
 fun test(text: String): Int{
